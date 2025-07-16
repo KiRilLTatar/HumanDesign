5eda7b69f22732b820fd6ac47be1737f787eeb41
ignat <ignatnkh7@gmail.com>
2025-07-16 17:45:39 +0300

    calc

--- core/views.py
+++ core/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'core/main.html')
+def calc(request):
+    return render(request, 'core/calc.html')
